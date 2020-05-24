29352dd0094fa345d6cfd94291735876f83ad5d0
Mathieu Boespflug <m@tweag.io>
2020-05-25 01:13:50 +0200

    Use netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,7 @@ their vision, from research to product delivery.
     "gatsby-redirect-from",
     "gatsby-plugin-meta-redirect",
     "gatsby-plugin-sass",
+    "gatsby-plugin-netlify",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
