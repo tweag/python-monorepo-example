260990ce0c491700d2753f224634eb3801e7ef28
Mathieu Boespflug <m@tweag.io>
2020-05-25 17:10:59 +0200

    Intersperse pattern images in blog archive

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,9 +5,14 @@ import Layout from "../components/layout"
 import { Card, TopCard } from "../components/blog-card"
 import SEO from "../components/seo"
 
+import pattern1 from "../images/post_pattern1.png"
+import pattern2 from "../images/post_pattern2.png"
+import pattern3 from "../images/post_pattern3.png"
+
 const BlogIndex = ({ data }) => {
   const topPost = data.allMarkdownRemark.edges[0]
   const posts = data.allMarkdownRemark.edges.slice(1)
+  const patterns = [pattern1, pattern2, pattern3]
   return (
     <Layout>
       <SEO title="Engineering Blog" />
@@ -20,8 +25,23 @@ const BlogIndex = ({ data }) => {
         <TopCard node={topPost.node} />
         <div className="services-section Blog-content viewport-section">
           <div className="post_container">
-            {posts.map(post => {
-              return <Card node={post.node} />
+            {posts.map((post, i) => {
+              // Display a pattern periodically.
+              const period = 6
+              const start = 3
+              const pattern = patterns[((i + start) / period) % patterns.length]
+              if ((i + start) % period === 0) {
+                return (
+                  <>
+                    <div className="post_content notop">
+                      <img src={pattern} alt="" />
+                    </div>
+                    <Card node={post.node} />
+                  </>
+                )
+              } else {
+                return <Card node={post.node} />
+              }
             })}
           </div>
         </div>
