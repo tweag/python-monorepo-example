2d51c9c3168c185ee2062d81e1e2151d53621bdc
Mathieu Boespflug <m@tweag.io>
2020-05-28 03:00:52 +0200

    Enable RSS feeds
    cc @guaraqe

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,104 @@ their vision, from research to product delivery.
         trackingId: `UA-47336061-2`,
       },
     },
-    // `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description:
+                    edge.node.frontmatter.description || edge.node.excerpt,
+                  date: edge.node.fields.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [fields___slug] }
+                  limit: 20
+                ) {
+                  edges {
+                    node {
+                      excerpt(pruneLength: 280)
+                      html
+                      fields {
+                        date
+                        slug
+                      }
+                      frontmatter {
+                        title
+                        description
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Tweag - Engineering blog",
+            match: "^/blog/",
+          },
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description:
+                    edge.node.frontmatter.description || edge.node.excerpt,
+                  date: edge.node.fields.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [fields___slug] }
+                  filter: { frontmatter: { tags: { in: ["nix"] } } }
+                  limit: 20
+                ) {
+                  edges {
+                    node {
+                      excerpt(pruneLength: 280)
+                      html
+                      fields {
+                        date
+                        slug
+                      }
+                      frontmatter {
+                        title
+                        description
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss-nix.xml",
+            title: "Tweag - Nix blog",
+            match: "^/blog/",
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
