tweag/python-monorepo-example

commit a662b6ab4454cd839fc9d7bbbb7eea461b2beea6

Replace Sass with CSS-in-JS (#518)
* Move to CSS-in-JS instead of Sass

For now we don't actually break up style declarations per-component.
We only get rid of Sass and use style embedded in JS using template
literals.

* Move footer styling out of global styles

* Give a name to colors

* Footer: special case >=1500 width screens, like previously

* Fix lint

Co-authored-by: guaraqe <mail@guaraqe.gq>

src/components/footer.js
@@ -1,103 +1,188 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import { Link } from "gatsby"
 
-import "./footer.css"
 import logo from "../images/tweag_logo_footer.svg"
 
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="container">
-        <div className="footer-col-area">
-          <div className="footer-col">
-            <div className="mail-area">
-              <strong className="mail-title">
-                Interested in working together?
-              </strong>
-              <Link to="/contact" className="btn">
-                Contact us
-              </Link>
-            </div>
-          </div>
-          <div className="footer-col">
-            <strong className="title">See our work</strong>
-            <ul className="footer-nav">
-              <li>
-                <Link to="/industry/biotech">Biotech</Link>
-              </li>
-              <li>
-                <Link to="/industry/fintech">Fintech</Link>
-              </li>
-              <li>
-                <Link to="/industry/autonomous">Autonomous vehicles</Link>
-              </li>
-              <li>
-                <Link to="/opensource">Open source</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <strong className="title">Stay connected</strong>
-            <ul className="footer-nav">
-              <li>
-                <a href="https://twitter.com/tweagio">Twitter</a>
-              </li>
-              <li>
-                <a href="https://github.com/tweag">GitHub</a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/company/tweag-i-o/">
-                  Linkedin
-                </a>
-              </li>
-              <li>
-                <a href="/rss.xml">RSS</a>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <strong className="title">About us</strong>
-            <ul className="footer-nav">
-              <li>
-                <Link to="/services">Services</Link>
-              </li>
-              <li>
-                <Link to="/blog">Blog</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-col tweag">
-            <strong className="f-logo">
-              <Link to="/">
-                <img src={logo} alt="Tweag" />
-              </Link>
-            </strong>
-          </div>
+const ContactUs = () => (
+  <Link
+    to="/contact"
+    sx={{
+      color: `white`,
+      borderColor: `white`,
+      fontSize: 1,
+      p: `5px 18px`,
+      height: `auto`,
+      "&:after": {
+        background: `white`,
+      },
+      "&:hover": {
+        color: `black`,
+      },
+    }}
+    className="btn"
+  >
+    Contact us
+  </Link>
+)
+
+const ColumnArea = props => (
+  <div
+    {...props}
+    sx={{
+      display: `flex`,
+      flexWrap: `wrap`,
+      justifyContent: `space-between`,
+      alignItems: `flex-start`,
+    }}
+  />
+)
+
+const Column = props => (
+  <div
+    {...props}
+    sx={{
+      position: `relative`,
+      width: [`50%`, `15%`],
+      m: [`0 0 20px`, `0 0 22px`],
+      p: [`0 10px`, `0`],
+    }}
+  />
+)
+
+const ColumnTitle = props => (
+  <strong
+    {...props}
+    sx={{
+      fontSize: 3,
+      lineHeight: `24px`,
+      fontWeight: `400`,
+      display: `block`,
+      m: [`0 0 10px`, `0 0 20px`],
+    }}
+  />
+)
+
+const Nav = props => (
+  <ul
+    {...props}
+    sx={{
+      m: 0,
+      p: 0,
+      listStyle: `none`,
+      position: `relative`,
+    }}
+  />
+)
+
+const NavItem = props => <li {...props} />
+
+const Footer = () => (
+  <footer
+    sx={{
+      fontSize: [0, 1, null, null, null, 2],
+      lineHeight: [`18px`, `24px`, null, null, null, `26px`],
+      position: `relative`,
+      bg: `black`,
+      color: `white`,
+      p: [`60px 0`, `80px 50px 0`, null, null, null, `60px 120px`],
+      width: `100%`,
+      "*::selection": {
+        bg: `rgba(255, 255, 255, 0.99)`,
+        color: `black`,
+      },
+      "*::-moz-selection": {
+        bg: `rgba(255, 255, 255, 0.99)`,
+        color: `black`,
+      },
+      a: {
+        color: `white`,
+      },
+    }}
+  >
+    <ColumnArea>
+      <Column>
+        <div className="mail-area">
+          <strong className="mail-title">
+            Interested in working together?
+          </strong>
+          <ContactUs />
         </div>
-        <div className="copyright-area">
-          <div className="copyright-col1">
-            <span className="copyright">
-              Tweag HQ &rarr; 207 Rue de Bercy &mdash; 75012 Paris &mdash;
-              France
-              <span>
-                <br />
-                <span>hello@tweag.io</span>
-              </span>
-            </span>
-          </div>
-          <div className="copyright-col2">
-            <span className="copyright">
-              &copy; 2020 Tweag.&nbsp;All rights reserved
-              <span>
-                <br />
-                <span>Privacy Policy</span>
-              </span>
-            </span>
-          </div>
-        </div>
+      </Column>
+      <Column>
+        <ColumnTitle>See our work</ColumnTitle>
+        <Nav>
+          <NavItem>
+            <Link to="/industry/biotech">Biotech</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/industry/fintech">Fintech</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/industry/autonomous">Autonomous vehicles</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/opensource">Open source</Link>
+          </NavItem>
+        </Nav>
+      </Column>
+      <Column>
+        <ColumnTitle>Stay connected</ColumnTitle>
+        <Nav>
+          <NavItem>
+            <a href="https://twitter.com/tweagio">Twitter</a>
+          </NavItem>
+          <NavItem>
+            <a href="https://github.com/tweag">GitHub</a>
+          </NavItem>
+          <NavItem>
+            <a href="https://www.linkedin.com/company/tweag-i-o/">Linkedin</a>
+          </NavItem>
+          <NavItem>
+            <a href="/rss.xml">RSS</a>
+          </NavItem>
+        </Nav>
+      </Column>
+      <Column>
+        <ColumnTitle>About us</ColumnTitle>
+        <Nav>
+          <NavItem>
+            <Link to="/services">Services</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/blog">Blog</Link>
+          </NavItem>
+        </Nav>
+      </Column>
+      <Column sx={{ paddingTop: `20px` }}>
+        <strong className="f-logo">
+          <Link to="/">
+            <img src={logo} alt="Tweag" />
+          </Link>
+        </strong>
+      </Column>
+    </ColumnArea>
+    <div className="copyright-area">
+      <div className="copyright-col1">
+        <span className="copyright">
+          Tweag HQ &rarr; 207 Rue de Bercy &mdash; 75012 Paris &mdash; France
+          <span>
+            <br />
+            <span>hello@tweag.io</span>
+          </span>
+        </span>
       </div>
-    </footer>
-  )
-}
+      <div className="copyright-col2">
+        <span className="copyright">
+          &copy; 2020 Tweag.&nbsp;All rights reserved
+          <span>
+            <br />
+            <span>Privacy Policy</span>
+          </span>
+        </span>
+      </div>
+    </div>
+  </footer>
+)
 
 export default Footer

gatsby-config.js
@@ -16,6 +16,8 @@ their vision, from research to product delivery.
     },
   },
   plugins: [
+    `gatsby-plugin-theme-ui`,
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -177,7 +179,6 @@ their vision, from research to product delivery.
     `gatsby-plugin-react-helmet`,
     `gatsby-redirect-from`,
     `gatsby-plugin-meta-redirect`,
-    `gatsby-plugin-sass`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-sitemap`,
   ],

src/components/footer.css
@@ -1,97 +1,0 @@
-.footer {
-  font-size: 18px;
-  line-height: 26px;
-  position: relative;
-  background: #000;
-  color: #fff;
-  padding: 80px 120px;
-  width: 100%;
-}
-
-.footer *::selection {
-  background: rgba(255, 255, 255, 0.99); /* WebKit/Blink Browsers */
-  color: #000;
-}
-
-.footer *::-moz-selection {
-  background: rgba(255, 255, 255, 0.99); /* Gecko Browsers */
-  color: #000;
-}
-
-.footer:before {
-  content: "";
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: -9999px;
-  background: #000;
-}
-
-.footer a {
-  color: #fff;
-}
-
-.footer .btn {
-  color: #fff;
-  border-color: #fff;
-  font-size: 16px;
-  padding: 5px 18px;
-  height: auto;
-}
-
-.footer .btn:after {
-  background: #fff;
-}
-
-.footer .btn:hover {
-  color: #000;
-}
-
-.footer-col-area {
-  position: relative;
-  display: -webkit-box;
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-wrap: wrap;
-  flex-wrap: wrap;
-  -webkit-box-align: start;
-  -ms-flex-align: start;
-  align-items: flex-start;
-  -webkit-box-pack: justify;
-  -ms-flex-pack: justify;
-  justify-content: space-between;
-}
-
-.footer-col {
-  position: relative;
-  width: 15%;
-  margin: 0 0 22px;
-}
-
-.footer-col .title {
-  font-size: 24px;
-  line-height: 24px;
-  font-weight: 400;
-  display: block;
-  margin: 0 0 20px;
-}
-
-.footer-col address {
-  font-style: normal;
-  display: block;
-  margin: 0;
-}
-
-.footer-nav,
-.social-links {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  position: relative;
-}
-
-.footer-nav li,
-.social-links li {
-  position: relative;
-}

src/components/layout-fullpage.js
@@ -1,11 +1,14 @@
 import React from "react"
 import ReactFullpage from "@fullpage/react-fullpage"
+import { Global } from "@emotion/core"
 
 import Header from "./header"
 import Footer from "./footer"
+import { globalStyles } from "../styles/global"
 
 import "normalize.css"
-import "./layout.scss"
+import "../fonts/Stratos.css"
+import "../fonts/icomoon.css"
 
 const FULLPAGE_LICENSE_KEY = `E12D894A-7B8349EE-B967F95D-3FF82929`
 
@@ -13,6 +16,7 @@ const LayoutFullPage = ({ children }) => {
   return (
     <div id="wrapper" className="home">
       <Header />
+      <Global styles={globalStyles} />
       <ReactFullpage
         licenseKey={FULLPAGE_LICENSE_KEY}
         responsiveWidth={769}

src/components/layout.js
@@ -1,15 +1,19 @@
 import React from "react"
+import { Global } from "@emotion/core"
 
 import Header from "./header"
 import Footer from "./footer"
+import { globalStyles } from "../styles/global"
 
 import "normalize.css"
-import "./layout.scss"
+import "../fonts/Stratos.css"
+import "../fonts/icomoon.css"
 
 const Layout = ({ children }) => {
   return (
     <div id="wrapper" className="inner">
       <Header />
+      <Global styles={globalStyles} />
       <main>{children}</main>
       <Footer />
     </div>

src/fonts/Stratos.css
@@ -1,0 +1,14 @@
+@font-face {
+  font-family: stratos;
+  src: url("./Stratos-Regular.woff") format("woff"),
+    url("./Stratos-Regular.woff2") format("woff2");
+  font-weight: 400;
+  font-style: normal;
+}
+@font-face {
+  font-family: stratos;
+  src: url("./Stratos-Bold.woff") format("woff"),
+    url("./Stratos-Bold.woff2") format("woff2");
+  font-weight: 700;
+  font-style: normal;
+}

src/fonts/icomoon.css
@@ -1,0 +1,7 @@
+@font-face {
+  font-family: icomoon;
+  src: url("./icomoon.woff") format("woff"),
+    url("./icomoon.woff2") format("woff2");
+  font-weight: 400;
+  font-style: normal;
+}

commit 50818dd90925a78125e30c18cee85d327acf3c15

Restore underline on hover in footer nav items
This was a regression introduced by #518.

src/components/footer.js
@@ -13,7 +13,7 @@ const ContactUs = () => (
       fontSize: 1,
       p: `5px 18px`,
       height: `auto`,
-      "&:after": {
+      "&::after": {
         background: `white`,
       },
       "&:hover": {
@@ -75,7 +75,34 @@ const Nav = props => (
   />
 )
 
-const NavItem = props => <li {...props} />
+const NavItem = props => (
+  <li
+    {...props}
+    sx={{
+      a: {
+        color: `white`,
+        position: `relative`,
+        overflow: `hidden`,
+        zIndex: 2,
+      },
+      "a::after": {
+        content: `""`,
+        position: `absolute`,
+        left: 0,
+        width: 0,
+        top: `20px`,
+        height: `2px`,
+        bottom: 0,
+        transition: `all 0.4s ease`,
+        background: `white`,
+      },
+      "a:hover::after": {
+        width: `100%`,
+        color: `white`,
+      },
+    }}
+  />
+)
 
 const Footer = () => (
   <footer
@@ -94,9 +121,6 @@ const Footer = () => (
       "*::-moz-selection": {
         bg: `rgba(255, 255, 255, 0.99)`,
         color: `black`,
-      },
-      a: {
-        color: `white`,
       },
     }}
   >