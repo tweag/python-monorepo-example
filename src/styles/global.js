6846ce27976c51bac66ad0979480f84632e9f39c
Mathieu Boespflug <m@tweag.io>
2020-06-27 10:22:10 +0200

    Use Percy to catch visual changes on the pages

--- src/styles/global.js
+++ src/styles/global.js
@@ -436,6 +436,24 @@ export const globalStyles = t => {
       opacity: 1;
       visibility: visible;
     }
+    @media only percy {
+      .section .visual-holder .image-holder,
+      .section .visual-holder .visual-caption,
+      .section .contactus-holder .image-col,
+      .section .contactus-holder .text-area,
+      .section .col-area h2,
+      .section .col-area .info-col,
+      .section .joinus-holder .text-wrap,
+      .section .joinus-holder .image-holder,
+      .section .vision-holder .text-col,
+      .section .vision-holder .image-holder,
+      .section .partners-area h2,
+      .section .partners-list li {
+        transform: none;
+        opacity: 1;
+        visibility: visible;
+      }
+    }
     .section01,
     .s_white {
       background: white;
@@ -2113,6 +2131,15 @@ export const globalStyles = t => {
         -ms-transform: none;
         transform: none;
       }
+      @media only percy {
+        .services-section .text-area,
+        .services-section .image-holder,
+        .services-section .text-area2 {
+          opacity: 1;
+          visibility: visible;
+          transform: none;
+        }
+      }
       .method-area .text-area {
         -webkit-transform: translateY(300px);
         -ms-transform: translateY(300px);
@@ -2142,6 +2169,14 @@ export const globalStyles = t => {
         -webkit-transform: none;
         -ms-transform: none;
         transform: none;
+      }
+      @media only percy {
+        .method-area .text-area,
+        .method-area .image-holder {
+          opacity: 1;
+          visibility: visible;
+          transform: none;
+        }
       }
       .about-section .text-area {
         -webkit-transform: translateY(300px);
@@ -2186,7 +2221,14 @@ export const globalStyles = t => {
         -webkit-transform: none;
         -ms-transform: none;
         transform: none;
-        /* margin-bottom: 60px; */
+      }
+      @media only percy {
+        .about-section .text-area,
+        .about-section .image-holder {
+          opacity: 1;
+          visibility: visible;
+          transform: none;
+        }
       }
       .w30 {
         width: 30% !important;
@@ -3270,6 +3312,11 @@ export const globalStyles = t => {
       }
       #___gatsby {
         overflow: hidden;
+      }
+    }
+    @media only percy {
+      .hide-in-percy {
+        visibility: hidden;
       }
     }
   `
