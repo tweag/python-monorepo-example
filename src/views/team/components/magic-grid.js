78474dbf79b7502d66e9999aff3f56875543780d
Bruno Gregório <bruno.gregorio.silva@outlook.com>
2021-10-18 16:25:37 -0300

    Visual milestone: desktop mode complete

--- src/views/team/components/magic-grid.js
+++ src/views/team/components/magic-grid.js
@@ -33,11 +33,56 @@ function parseCssVariables({ gap, margin, columns }) {
   return result
 }
 
+/**
+ * @param {MouseEvent} event
+ */
+function shuffleButtonAnimationHandler(event) {
+  const button = event.target
+  button.classList.add(styles.rotateShuffleButton)
+  button.addEventListener(
+    `animationend`,
+    () => button.classList.remove(styles.rotateShuffleButton),
+    { once: true }
+  )
+}
+
+const ShuffleButton = () => {
+  return (
+    <a className={styles.shuffleButton} onClick={shuffleButtonAnimationHandler}>
+      Roll again
+    </a>
+  )
+}
+
+const SearchBar = ({ placeholder }) => {
+  return (
+    <input
+      name="search"
+      id="searchFilter"
+      type="text"
+      className={styles.searchBar}
+      placeholder={placeholder}
+    />
+  )
+}
+
 const MagicGrid = ({ children, gap, margin, columns }) => {
   const sizingVariables = parseCssVariables({ gap, margin, columns })
   return (
-    <div className={styles.magicGrid} style={sizingVariables}>
-      {children}
+    <div className={styles.magicGridContainer}>
+      <div
+        className={styles.actionBar}
+        sx={{
+          pl: [`15px`, `15px`, `60px`, `60px`, `60px`, `60px`, `120px`],
+          pr: [`15px`, `15px`, `60px`, `60px`, `60px`, `60px`, `120px`],
+        }}
+      >
+        <SearchBar placeholder={`Search for a name or a skill`} />
+        <ShuffleButton />
+      </div>
+      <div className={styles.magicGrid} style={sizingVariables}>
+        {children}
+      </div>
     </div>
   )
 }
