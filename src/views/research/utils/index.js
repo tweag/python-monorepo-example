bc287478c180d95c33d336d7ac00ff832b3a2ba4
Solène Rapenne <solene.rapenne@tweag.io>
2022-08-18 09:39:26 +0200

    apply prettier on JS code

--- src/views/research/utils/index.js
+++ src/views/research/utils/index.js
@@ -124,18 +124,18 @@ export function classifyArray(arrayToClassify, classifierFunction) {
 export function getFullMonth(monthNumberString) {
   // Be careful when using Intl object on node, it doesn't work properly
   const months = {
-    "0": `January`,
-    "1": `February`,
-    "2": `March`,
-    "3": `April`,
-    "4": `May`,
-    "5": `June`,
-    "6": `July`,
-    "7": `August`,
-    "8": `September`,
-    "9": `October`,
-    "10": `November`,
-    "11": `December`,
+    0: `January`,
+    1: `February`,
+    2: `March`,
+    3: `April`,
+    4: `May`,
+    5: `June`,
+    6: `July`,
+    7: `August`,
+    8: `September`,
+    9: `October`,
+    10: `November`,
+    11: `December`,
   }
   return months[monthNumberString]
 }
