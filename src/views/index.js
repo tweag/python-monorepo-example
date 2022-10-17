fb1fed4e73655aefb1d9f22734ab6856fac23034
alexumeh <alexcumeh@gmail.com>
2022-10-17 01:08:33 +0100

    updated cookie banner script in ../views/index.js

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
 
+import { Script } from "gatsby"
 import { FullPageLayout } from "../layouts"
 import { SEO, CallToActionFooter, Footer } from "../components"
 
@@ -9,6 +10,13 @@ import { Section1, Section2, Section3, Section4, Section5 } from "./components"
 const IndexPage = () => {
   return (
     <FullPageLayout>
+      <Script
+        src="https://cdn.cookielaw.org/scripttemplates/otSDKStub.js"
+        type="text/javascript"
+        charset="UTF-8"
+        data-domain-script="3a05f346-a11c-458d-9bec-8c7fb98aa3aa"
+      ></Script>
+      {/* <Script type="text/javascript">function OptanonWrapper()</Script> */}
       <SEO title="Software innovation lab" />
       <div className="section s_white">
         <Section1 />
