54806ec32e22aedaa254a26e30f2b59ecf035134
Douglas Massolari <douglas.massolari@tweag.io>
2023-02-17 19:28:00 -0300

    fix(navigation): prevent menu bar of wrapping
    - Use vw unit on font sizes, so it will shrink with screen size
    - Refactor dropdown animation to fix translate transition

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,7 +6,7 @@ import { Link } from "gatsby"
 import blackLogo from "../images/logo_tweag_modus_header.png"
 
 const navLinkClassName = `button min-1__button-link-bottom-lined`
-const menuItemFontSize = [`27px`, `1.5vw`, `1.3vw`]
+const menuItemFontSize = [`27px`, `1.7vw`, `1.4vw`]
 
 const Logo = () => (
   <Link to="/">
@@ -38,24 +38,28 @@ type DropDownProps = {
 }
 const Dropdown: React.FC<DropDownProps> = ({ title, items }) => {
   const dropDownClass = `header-drop-down-transition__show-in`
+  const transition = `opacity 0.4s ease, transform ease 0.4s`
+
   return (
     <Box
       sx={{
-        mx: [0, `15px`, `25px`],
-        mb: [`15px`, 0],
+        mx: [0, `0.7vw`],
         textAlign: [`center`, `start`],
         bg: `inherit`,
-          display: [`none`, `flex`],
+        display: `flex`,
+        flexDirection: `column`,
         [`.${dropDownClass}`]: {
           opacity: 0,
-          transform: [null, `translateY(-1000px)`],
-          transition: [null, `opacity 0.4s ease, transform 0.1s ease 0.4s`],
+          visibility: `hidden`,
+          display: [`none`, `inherit`],
+          transition,
         },
-          display: `flex`,
         [`&:hover .${dropDownClass}, &:focus-within .${dropDownClass}`]: {
           opacity: 1,
-          transform: [null, `translateY(0px)`],
-          transition: [null, `opacity 0.4s ease`],
+          visibility: `visible`,
+          display: `inherit`,
+          transform: `translateY(1.7vw)`,
+          transition,
         },
       }}
     >
@@ -63,7 +67,7 @@ const Dropdown: React.FC<DropDownProps> = ({ title, items }) => {
         as="div"
         sx={{
           minWidth: [`fit-content`],
-          lineHeight: [1.1],
+          lineHeight: [1.8, 1.1],
           fontSize: menuItemFontSize,
           cursor: `pointer`,
           userSelect: `none`,
@@ -89,10 +93,9 @@ const Dropdown: React.FC<DropDownProps> = ({ title, items }) => {
           },
           position: [null, `absolute`],
           flexDirection: `column`,
-          pt: [`15px`, `10px`],
-          pb: [0, `10px`],
-          mx: [`-15px`],
-          px: [`15px`],
+          pb: [0, `15px`],
+          p: `15px`,
+          pl: 0,
           alignItems: [`center`, `start`],
         }}
       >
@@ -102,7 +105,7 @@ const Dropdown: React.FC<DropDownProps> = ({ title, items }) => {
             to={to}
             customSx={{
               mb: `10px`,
-              fontSize: [`23px`, `16px`, `18px`],
+              fontSize: [`23px`, `1.3vw`],
             }}
             customClassName={navLinkClassName}
           >
@@ -201,7 +204,6 @@ function Header({ inverted, fullpage = false }) {
   return (
     <Flex
       sx={{
-        flexWrap: `wrap`,
         p: [`15px`, `25px`, `35px`],
         justifyContent: [`space-between`],
         alignItems: `center`,
@@ -219,7 +221,7 @@ function Header({ inverted, fullpage = false }) {
     >
       <Box
         sx={{
-          width: [`175px`, `275px`, `300px`],
+          width: [`175px`, `25vw`, `300px`],
           mt: [0, `16px`, fullpage ? 0 : null],
           pb: [0, `7px`],
           transform: [null, null, fullpage ? `scale(0.9)` : null],
@@ -236,14 +238,13 @@ function Header({ inverted, fullpage = false }) {
           mt: [0, `16px`, fullpage ? 0 : null],
           alignItems: [`center`, `flex-end`],
           height: [`100vh`, `auto`],
-          opacity: [navbarState ? 1 : 0, 1],
-          zIndex: [navbarState ? 99 : -999, 99],
           position: [`absolute`, `unset`],
           top: [`45px`, 0],
           right: [0],
           left: [0],
           pt: [`66px`, 0],
           background: [`var(--bg-color)`, `inherit`],
+          gap: [`15px`, 0],
         }}
       >
         <NavLink
@@ -252,7 +253,6 @@ function Header({ inverted, fullpage = false }) {
             mx: [0, `15px`, `25px`],
             mr: [0, `15px`, `25px`],
             ml: [0],
-            mb: [`15px`, 0],
           }}
           customClassName={navLinkClassName}
         >
@@ -290,10 +290,9 @@ function Header({ inverted, fullpage = false }) {
             to={to}
             isExternal={isExternal}
             customSx={{
-              mx: [0, `15px`, `25px`],
+              mx: [0, `0.5vw`, `1vw`],
               ...(i === arr.length - 1 ? { mr: [0] } : {}),
               minWidth: [`fit-content`],
-              mb: [`15px`, 0],
             }}
           >
             {title}
