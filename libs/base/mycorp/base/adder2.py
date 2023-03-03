5a89ee5e90e4836689c7832de402f958482b1e91
Clément Hurlin <clement.hurlin@tweag.io>
2023-03-03 10:49:35 +0100

    Add project

---
+++ libs/base/mycorp/base/adder2.py
@@ -0,0 +1,11 @@
+def add2(x: int, y: int) -> int:
+    """
+    Adds two integers
+
+    Args:
+        x: the left operand
+        y: the right operand
+    Returns:
+        The sum of x and y
+    """
+    return x + y
---
+++ libs/fancy/mycorp/fancy/adder3.py
@@ -0,0 +1,15 @@
+from mycorp import base
+
+
+def add3(x: int, y: int, z: int) -> int:
+    """
+    Adds three integers
+
+    Args:
+        x: the left operand
+        y: the middle operand
+        z: the right operand
+    Returns:
+        The sum of x, y, and z
+    """
+    return base.adder2.add2(base.adder2.add2(x, y), z)
