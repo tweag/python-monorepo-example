48bd392132172571d32ab6e1ef96b4b718529677
Clément Hurlin <clement.hurlin@tweag.io>
2023-04-24 17:48:57 +0200

    Adapt existing libraries to match template

---
+++ libs/fancy/tests/test_example.py
@@ -0,0 +1,8 @@
+"""
+An example of test module provided by the project template.
+"""
+
+
+def test_something():
+    """An example of test found and run by pytest."""
+    raise NotImplementedError("No test was implemented. Either remove this test or implement it.")
