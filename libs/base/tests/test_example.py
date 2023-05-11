92b5142521a88cb94b4a9ede87907c803fcff7fc
Clément Hurlin <clement.hurlin@tweag.io>
2023-05-11 11:27:48 +0200

    Make tests of libs/base pass

--- libs/base/tests/test_example.py
+++ libs/base/tests/test_example.py
@@ -1,8 +1,24 @@
 """
-An example of test module provided by the project template.
+Tests of adder2
 """
 
+from mycorp import base
 
-def test_something():
-    """An example of test found and run by pytest."""
-    raise NotImplementedError("No test was implemented. Either remove this test or implement it.")
+
+def test_zero() -> None:
+    """Tests the neutral element"""
+    assert base.adder2.add2(0, 0) == 0
+    assert base.adder2.add2(0, 3) == 3
+    assert base.adder2.add2(0, 3) == base.adder2.add2(3, 0)
+
+
+def test_some() -> None:
+    """Some unit tests"""
+    assert base.adder2.add2(1, 3) == 4
+    assert base.adder2.add2(1, 3) == base.adder2.add2(3, 1)
+    assert base.adder2.add2(100, 2) == (base.adder2.add2(100, 1) + base.adder2.add2(1, 0))
+
+
+# In a true repository we would add property tests here,
+# using hypothesis:
+# https://hypothesis.readthedocs.io/en/latest/
